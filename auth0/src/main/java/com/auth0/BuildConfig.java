93a08b0551d71bd49050dff82323114327690f41
Hernan Zalazar <hernanzalazar@gmail.com>
2016-03-18 14:29:24 -0300

    Next development iteration

--- auth0/src/main/java/com/auth0/BuildConfig.java
+++ auth0/src/main/java/com/auth0/BuildConfig.java
@@ -7,7 +7,7 @@ abstract class BuildConfig {
     /**
      * Version of auth0-java
      */
-    public static final String VERSION = "0.1.0";
+    public static final String VERSION = "0.2.0-SNAPSHOT";
     /**
      * Name of the library auth0-java
      */
