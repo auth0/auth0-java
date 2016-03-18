a56fa2c4e0943d74935bce10a1e925888a1942bb
Hernan Zalazar <hernanzalazar@gmail.com>
2016-03-18 14:27:22 -0300

    Release 0.1.0

--- auth0/src/main/java/com/auth0/BuildConfig.java
+++ auth0/src/main/java/com/auth0/BuildConfig.java
@@ -2,12 +2,12 @@ package com.auth0;
 
 abstract class BuildConfig {
 
-    private BuildConfig() {};
+    private BuildConfig() {}
 
     /**
      * Version of auth0-java
      */
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "0.1.0";
     /**
      * Name of the library auth0-java
      */
