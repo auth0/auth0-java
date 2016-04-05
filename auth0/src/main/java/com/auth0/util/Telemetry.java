97c7053437f9bda633331a1cc8e4fe119a6bda64
Hernan Zalazar <hernanzalazar@gmail.com>
2016-04-05 11:36:54 -0300

    Update telemetry classes

--- auth0/src/main/java/com/auth0/util/Telemetry.java
+++ auth0/src/main/java/com/auth0/util/Telemetry.java
@@ -1,10 +1,51 @@
 package com.auth0.util;
 
-public interface Telemetry {
-    String NAME_KEY = "name";
-    String VERSION_KEY = "version";
-    String HEADER_NAME = "Auth0-Client";
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-    void usingLibrary(String name, String version);
-    String getValue();
+import java.util.HashMap;
+import java.util.Map;
+
+public class Telemetry {
+    public static final String HEADER_NAME = "Auth0-Client";
+
+    private static final String NAME_KEY = "name";
+    private static final String VERSION_KEY = "version";
+    private static final String LIB_VERSION_KEY = "lib_version";
+
+    private final String name;
+    private final String version;
+    private final String libraryVersion;
+
+    public Telemetry(String name, String version) {
+        this(name, version, null);
+    }
+
+    public Telemetry(String name, String version, String libraryVersion) {
+        this.name = name;
+        this.version = version;
+        this.libraryVersion = libraryVersion;
+    }
+
+    public String getValue() {
+        Map<String, String> values = new HashMap<>();
+        if (name != null) {
+            values.put(NAME_KEY, name);
+        }
+        if (version != null) {
+            values.put(VERSION_KEY, version);
+        }
+        if (libraryVersion != null) {
+            values.put(LIB_VERSION_KEY, libraryVersion);
+        }
+        if (values.isEmpty()) {
+            return null;
+        }
+        try {
+            String json = new ObjectMapper().writeValueAsString(values);
+            return Base64.encodeUrlSafe(json);
+        } catch (JsonProcessingException e) {
+            return null;
+        }
+    }
 }
