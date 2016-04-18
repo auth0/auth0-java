eded9fd476d6acf548638efbd8b5e421db04114e
Hernan Zalazar <hernan.zalazar@gmail.com>
2016-04-18 20:40:26 -0300

    Release 0.2.0

--- auth0/src/main/java/com/auth0/BuildConfig.java
+++ auth0/src/main/java/com/auth0/BuildConfig.java
@@ -7,7 +7,7 @@ abstract class BuildConfig {
     /**
      * Version of auth0-java
      */
-    public static final String VERSION = "0.2.0-SNAPSHOT";
+    public static final String VERSION = "0.2.0";
     /**
      * Name of the library auth0-java
      */
