ad50914b328e286551693092bfc256d30713b417
James Anderson <jim.anderson@auth0.com>
2020-05-21 11:34:03 -0500

    Remove assertNotEmpty method for arrays since it is no longer needed

--- src/main/java/com/auth0/utils/Asserts.java
+++ src/main/java/com/auth0/utils/Asserts.java
@@ -46,21 +46,4 @@ public abstract class Asserts {
             throw new IllegalArgumentException(String.format("'%s' cannot be empty!", name));
         }
     }
-
-    /**
-     * Asserts that an array is not null and has at least one item.
-     *
-     * @param <T> the object type.
-     * @param value the value to check.
-     * @param name the name of the parameter, used when creating the exception message.
-     * @throws IllegalArgumentException if the value is null or has length of zero.
-     */
-    public static <T> void assertNotEmpty(T[] value, String name) {
-        if (value == null) {
-            throw new IllegalArgumentException(String.format("'%s' cannot be null!", name));
-        }
-        if (value.length == 0) {
-            throw new IllegalArgumentException(String.format("'%s' cannot be empty!", name));
-        }
-    }
 }
