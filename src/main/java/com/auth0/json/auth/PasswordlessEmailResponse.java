a1a675f09afa40d11aca874fba522cfa40d9dd3d
James Anderson <jim.anderson@auth0.com>
2020-10-14 15:40:41 -0500

    Response model JavaDocs

--- src/main/java/com/auth0/json/auth/PasswordlessEmailResponse.java
+++ src/main/java/com/auth0/json/auth/PasswordlessEmailResponse.java
@@ -4,6 +4,9 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+/**
+ * Represents the successful response when initiating the passwordless flow via email.
+ */
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class PasswordlessEmailResponse {
@@ -15,16 +18,31 @@ public class PasswordlessEmailResponse {
     @JsonProperty("email_verified")
     private Boolean emailVerified;
 
+    /**
+     * The Identifier of this request.
+     *
+     * @return the identifier of this request.
+     */
     @JsonProperty("_id")
     public String getId() {
         return id;
     }
 
+    /**
+     * Gets the email to which the code or link was sent to.
+     *
+     * @return the email to which the code or link was sent to.
+     */
     @JsonProperty("email")
     public String getEmail() {
         return email;
     }
 
+    /**
+     * Whether the email address has been verified (true) or has not been verified (false).
+     *
+     * @return whether the email address has been verified (true) or has not been verified (false).
+     */
     @JsonProperty("email_verified")
     public Boolean isEmailVerified() {
         return emailVerified;
--- src/main/java/com/auth0/json/auth/PasswordlessSmsResponse.java
+++ src/main/java/com/auth0/json/auth/PasswordlessSmsResponse.java
@@ -4,6 +4,9 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+/**
+ * Represents the successful response when initiating the passwordless flow via SMS.
+ */
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class PasswordlessSmsResponse {
@@ -16,21 +19,41 @@ public class PasswordlessSmsResponse {
     @JsonProperty("request_language")
     private String requestLanguage;
 
+    /**
+     * The Identifier of this request.
+     *
+     * @return the identifier of this request.
+     */
     @JsonProperty("_id")
     public String getId() {
         return id;
     }
 
+    /**
+     * Gets the phone number the code was sent to.
+     *
+     * @return the phone number the code was sent to.
+     */
     @JsonProperty("phone_number")
     public String getPhoneNumber() {
         return phoneNumber;
     }
 
+    /**
+     * Whether the phone number has been verified (true) or has not been verified (false).
+     *
+     * @return whether the phone number has been verified (true) or has not been verified (false).
+     */
     @JsonProperty("phone_verified")
     public Boolean isPhoneVerified() {
         return phoneVerified;
     }
 
+    /**
+     * The language of the message sent, if set.
+     *
+     * @return The language of the message sent, or null if not changed from the default.
+     */
     @JsonProperty("request_language")
     public String getRequestLanguage() {
         return requestLanguage;
