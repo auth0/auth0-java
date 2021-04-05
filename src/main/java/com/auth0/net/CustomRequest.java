3298cfd46d54fc555232a0621e500ee9e0577d20
Jim Anderson <jim.anderson@auth0.com>
2021-04-05 11:12:26 -0500

    Fix `NoSuchMethodError` when projects have OkHttp 3 dependency (#342)
    * Use deprecated methods
    
    * Update README and add comments

--- src/main/java/com/auth0/net/CustomRequest.java
+++ src/main/java/com/auth0/net/CustomRequest.java
@@ -42,12 +42,15 @@ public class CustomRequest<T> extends ExtendedBaseRequest<T> implements Customiz
     }
 
     @Override
+    @SuppressWarnings("deprecation")
     protected RequestBody createRequestBody() throws IOException {
         if (body == null && parameters.isEmpty()) {
             return null;
         }
         byte[] jsonBody = mapper.writeValueAsBytes(body != null ? body : parameters);
-        return RequestBody.create(jsonBody, MediaType.parse(CONTENT_TYPE_APPLICATION_JSON));
+        // Use OkHttp v3 signature to ensure binary compatibility between v3 and v4
+        // https://github.com/auth0/auth0-java/issues/324
+        return RequestBody.create(MediaType.parse(CONTENT_TYPE_APPLICATION_JSON), jsonBody);
     }
 
     @Override
