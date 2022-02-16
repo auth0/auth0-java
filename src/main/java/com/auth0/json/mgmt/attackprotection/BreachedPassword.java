56786856b00eb600fd6d1d5e0e950d6ff44d78a2
Jim Anderson <jim.anderson@auth0.com>
2022-02-16 16:37:18 -0600

    [SDK-3116] Add support for Attack Protection APIs (#405)

---
+++ src/main/java/com/auth0/json/mgmt/attackprotection/BreachedPassword.java
@@ -0,0 +1,88 @@
+package com.auth0.json.mgmt.attackprotection;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
+/**
+ * Represents the Breached Password entity.
+ *
+ * @see com.auth0.client.mgmt.AttackProtectionEntity
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class BreachedPassword {
+
+    @JsonProperty("enabled")
+    private Boolean enabled;
+    @JsonProperty("method")
+    private String method;
+    @JsonProperty("shields")
+    private List<String> shields;
+    @JsonProperty("admin_notification_frequency")
+    private List<String> adminNotificationFrequency;
+
+    /**
+     * @return whether or not breached password detection is active.
+     */
+    public Boolean getEnabled() {
+        return enabled;
+    }
+
+    /**
+     * Sets whether or not breached password detection is active.
+     * @param enabled whether or not breached password detection is active.
+     */
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    /**
+     * @return the subscription level for breached password detection methods.
+     */
+    public String getMethod() {
+        return method;
+    }
+
+    /**
+     * Sets the subscription level for breached password detection methods.
+     * @param method the subscription level for breached password detection methods.
+     */
+    public void setMethod(String method) {
+        this.method = method;
+    }
+
+    /**
+     * @return the action to take when a breached password is detected.
+     */
+    public List<String> getShields() {
+        return shields;
+    }
+
+    /**
+     * Sets the action to take when a breached password is detected.
+     * @param shields the action to take when a breached password is detected.
+     */
+    public void setShields(List<String> shields) {
+        this.shields = shields;
+    }
+
+    /**
+     * @return the frequency email notifications are sent.
+     */
+    public List<String> getAdminNotificationFrequency() {
+        return adminNotificationFrequency;
+    }
+
+    /**
+     * Sets the frequency email notifications are sent.
+     * @param adminNotificationFrequency the frequency email notifications are sent.
+     */
+    public void setAdminNotificationFrequency(List<String> adminNotificationFrequency) {
+        this.adminNotificationFrequency = adminNotificationFrequency;
+    }
+
+
+}
---
+++ src/test/java/com/auth0/json/mgmt/attackprotection/BruteForceConfigurationTest.java
@@ -0,0 +1,60 @@
+package com.auth0.json.mgmt.attackprotection;
+
+import com.auth0.json.JsonMatcher;
+import com.auth0.json.JsonTest;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class BruteForceConfigurationTest extends JsonTest<BruteForceConfiguration> {
+
+    String json = "{\n" +
+        "  \"enabled\": false,\n" +
+        "  \"shields\": [\n" +
+        "    \"block\",\n" +
+        "    \"user_notification\"\n" +
+        "  ],\n" +
+        "  \"allowlist\": [\n" +
+        "    \"143.204.0.105\",\n" +
+        "    \"2600:9000:208f:ca00:d:f5f5:b40:93a1\"\n" +
+        "  ],\n" +
+        "  \"mode\": \"count_per_identifier_and_ip\",\n" +
+        "  \"max_attempts\": 10\n" +
+        "}";
+
+    @Test
+    public void shouldSerialize() throws Exception {
+        BruteForceConfiguration bruteForceConfiguration = new BruteForceConfiguration();
+        bruteForceConfiguration.setEnabled(true);
+        bruteForceConfiguration.setMaxAttempts(10);
+        bruteForceConfiguration.setMode("count_per_identifier_and_ip");
+        bruteForceConfiguration.setAllowList(Arrays.asList("123", "456"));
+        bruteForceConfiguration.setShields(Arrays.asList("user_notification", "block"));
+
+        String json = toJSON(bruteForceConfiguration);
+        assertThat(json, JsonMatcher.hasEntry("enabled", true));
+        assertThat(json, JsonMatcher.hasEntry("max_attempts", 10));
+        assertThat(json, JsonMatcher.hasEntry("mode", "count_per_identifier_and_ip"));
+        assertThat(json, JsonMatcher.hasEntry("allowlist", Arrays.asList("123", "456")));
+        assertThat(json, JsonMatcher.hasEntry("shields", Arrays.asList("user_notification", "block")));
+    }
+
+    @Test
+    public void shouldDeserialize() throws Exception {
+        BruteForceConfiguration bruteForceConfiguration = fromJSON(json, BruteForceConfiguration.class);
+
+        assertThat(bruteForceConfiguration, notNullValue());
+        assertThat(bruteForceConfiguration.getEnabled(), is(false));
+        assertThat(bruteForceConfiguration.getMode(), is("count_per_identifier_and_ip"));
+        assertThat(bruteForceConfiguration.getMaxAttempts(), is(10));
+        assertThat(bruteForceConfiguration.getShields(), is(notNullValue()));
+        assertThat(bruteForceConfiguration.getShields().size(), is(2));
+        assertThat(bruteForceConfiguration.getShields(), contains("block", "user_notification"));
+        assertThat(bruteForceConfiguration.getAllowList(), is(notNullValue()));
+        assertThat(bruteForceConfiguration.getAllowList().size(), is(2));
+        assertThat(bruteForceConfiguration.getAllowList(), contains("143.204.0.105", "2600:9000:208f:ca00:d:f5f5:b40:93a1"));
+    }
+}
