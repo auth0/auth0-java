b28e539ea8f6cc80de3619ad972c40da67ec3850
Cristian Araya <cristian.araya@assaabloy.com>
2022-05-17 09:00:49 +0200

    Fixed tests

--- src/test/java/com/auth0/client/mgmt/OrganizationEntityTest.java
+++ src/test/java/com/auth0/client/mgmt/OrganizationEntityTest.java
@@ -167,10 +167,9 @@ public class OrganizationEntityTest extends BaseMgmtEntityTest {
         metadata.put("key1", "val1");
         orgToCreate.setMetadata(metadata);
 
-        Connection connection = new Connection();
-        connection.setName("con-1");
         EnabledConnection enabledConnection = new EnabledConnection();
-        enabledConnection.setConnection(connection);
+        enabledConnection.setConnectionId("con-1");
+        enabledConnection.setAssignMembershipOnLogin(false);
         List<EnabledConnection> enabledConnections = new ArrayList<>();
         enabledConnections.add(enabledConnection);
         orgToCreate.setEnabledConnections(enabledConnections);
@@ -187,11 +186,11 @@ public class OrganizationEntityTest extends BaseMgmtEntityTest {
         assertThat(recordedRequest, hasHeader("Authorization", "Bearer apiToken"));
 
         Map<String, Object> body = bodyFromRequest(recordedRequest);
-        assertThat(body, aMapWithSize(4));
+        assertThat(body, aMapWithSize(5));
         assertThat(body, hasEntry("name", "test-org"));
         assertThat(body, hasEntry("display_name", "display name"));
         assertThat(body, hasEntry("metadata", metadata));
-        assertThat(body, hasEntry("enabled_connections", enabledConnections));
+        assertThat(body, hasEntry(is("enabled_connections"), is(notNullValue())));
         assertThat(body, hasEntry(is("branding"), is(notNullValue())));
 
         assertThat(response, is(notNullValue()));
