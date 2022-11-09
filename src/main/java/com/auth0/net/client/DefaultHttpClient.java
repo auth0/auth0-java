9c5483546b7125f279ccf5f6f5ee38c4efcd5501
Jim Anderson <jimranderson@gmail.com>
2022-11-09 13:29:23 -0600

    cleanup DefaultHttpClient

--- src/main/java/com/auth0/net/client/DefaultHttpClient.java
+++ src/main/java/com/auth0/net/client/DefaultHttpClient.java
@@ -11,14 +11,13 @@ import org.jetbrains.annotations.NotNull;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 
-// TODO allow creation using existing OkHttp client (use client's builder!)!
 public class DefaultHttpClient implements HttpClient {
 
     private final okhttp3.OkHttpClient client;
-//    private final HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
 
     public static DefaultHttpClient.Builder newBuilder() {
         return new DefaultHttpClient.Builder();
@@ -33,10 +32,127 @@ public class DefaultHttpClient implements HttpClient {
         if (builder.telemetryEnabled) {
             clientBuilder.addInterceptor(new TelemetryInterceptor());
         }
-//        for (Interceptor i : builder.interceptors) {
-//            clientBuilder.addInterceptor(i);
-//        }
         client = clientBuilder.build();
+    }
+
+    // TODO accept params?
+    @Override
+    @SuppressWarnings("deprecation")
+    public HttpResponse makeRequest(HttpRequest request) throws IOException {
+        Request okRequest = buildRequest(request);
+
+        // execute
+        // TODO ensure response is closed
+        Response okResponse = client.newCall(okRequest).execute();
+
+        // return an Auth0 HttpResponse
+        return buildResponse(okResponse);
+    }
+
+    private Request buildRequest(HttpRequest a0Request) {
+        // TODO only use body on request methods that support it
+        // TODO not use null?
+        RequestBody okBody = addBody(a0Request);
+
+        okhttp3.Request.Builder builder = new okhttp3.Request.Builder()
+            .url(a0Request.getUrl())
+            .method(a0Request.getMethod().toString(), okBody);
+        for (Map.Entry<String, String> e : a0Request.getHeaders().entrySet()) {
+            builder.addHeader(e.getKey(), e.getValue());
+        }
+
+        return builder.build();
+    }
+
+    private HttpResponse buildResponse(Response okResponse) throws IOException {
+        Headers okHeaders = okResponse.headers();
+        Map<String, String> headers = new HashMap<>();
+        for (int i = 0; i < okHeaders.size(); i++) {
+            headers.put(okHeaders.name(i), okHeaders.value(i));
+        }
+
+        return HttpResponse.newBuilder()
+            .code(okResponse.code())
+            .body(Objects.nonNull(okResponse.body()) ? okResponse.body().string() : null)
+            .headers(headers)
+            .build();
+    }
+
+    @Override
+    @SuppressWarnings("deprecation")
+    public CompletableFuture<HttpResponse> makeRequestAsync(HttpRequest request) {
+        final CompletableFuture<HttpResponse> future = new CompletableFuture<>();
+
+        Request okRequest = buildRequest(request);
+
+
+        // Need to create with or without body
+
+//        RequestBody okBody = request.getBody() != null ?
+//            // TODO put media type on HttpRequestBody
+//            RequestBody.create(MediaType.parse("application/json"), request.getBody().getContent())
+//            : null;
+
+//        RequestBody okBody = request.getBody() != null ?
+//            RequestBody.create(MediaType.parse("application/json"), request.getBody())
+//            : null;
+
+
+//        okhttp3.Request.Builder builder = new okhttp3.Request.Builder()
+//            .url(request.getUrl())
+//            .method(request.getMethod().toString(), okBody);
+//        for (Map.Entry<String, String> e : request.getHeaders().entrySet()) {
+//            builder.addHeader(e.getKey(), e.getValue());
+//        }
+
+        // execute
+        client.newCall(okRequest).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                future.completeExceptionally(new Auth0Exception("Failed to execute request", e));
+            }
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                try {
+                    // TODO consider what to do on IOException (or other errors)
+                    //  when building the response
+                    future.complete(buildResponse(response));
+                } catch (Auth0Exception e) {
+                    future.completeExceptionally(e);
+                }
+            }
+        });
+
+        return future;
+    }
+
+    @SuppressWarnings("deprecation")
+    private RequestBody addBody(HttpRequest request) {
+        // null body added to request results in request without body
+        if (Objects.isNull(request.getBody())) {
+            return null;
+        }
+
+        HttpRequestBody body = request.getBody();
+        RequestBody okBody;
+
+        if (Objects.nonNull(body.getMultipartRequestBody())) {
+            HttpMultipartRequestBody multipartRequestBody = body.getMultipartRequestBody();
+            MultipartBody.Builder bodyBuilder = new MultipartBody.Builder()
+                .setType(MultipartBody.FORM);
+            if (Objects.nonNull(multipartRequestBody.getFilePart())) {
+                bodyBuilder.addFormDataPart(multipartRequestBody.getFilePart().getPartName(),
+                    multipartRequestBody.getFilePart().getFile().getName(),
+                    RequestBody.create(MediaType.parse(multipartRequestBody.getFilePart().getMediaType()), multipartRequestBody.getFilePart().getFile()));
+            }
+            multipartRequestBody.getParts().forEach(bodyBuilder::addFormDataPart);
+            okBody = bodyBuilder.build();
+        } else {
+            okBody = RequestBody.create(MediaType.parse("application/json"), request.getBody().getContent());
+        }
+
+        return okBody;
     }
 
     private void configureLogging(okhttp3.OkHttpClient.Builder clientBuilder, LoggingOptions loggingOptions) {
@@ -90,158 +206,6 @@ public class DefaultHttpClient implements HttpClient {
         }
     }
 
-    // TODO accept params?
-    @Override
-    @SuppressWarnings("deprecation")
-    public HttpResponse makeRequest(HttpRequest request) throws IOException {
-        // Build okHtp3 request
-
-        // Need to create with or without body
-
-
-        // TODO only use body on request methods that support it
-        // TODO not use null?
-        RequestBody okBody = addBody(request);
-
-        okhttp3.Request.Builder builder = new okhttp3.Request.Builder()
-            .url(request.getUrl())
-            .method(request.getMethod().toString(), okBody);
-        for (Map.Entry<String, String> e : request.getHeaders().entrySet()) {
-            builder.addHeader(e.getKey(), e.getValue());
-        }
-
-        // execute
-        Response okResponse = client.newCall(builder.build()).execute();
-
-        // TODO add response headers
-
-        // return an Auth0 HttpResponse
-        Headers okHeaders = okResponse.headers();
-        Map<String, String> headers = new HashMap<>();
-        for (int i = 0; i < okHeaders.size(); i++) {
-            headers.put(okHeaders.name(i), okHeaders.value(i));
-        }
-        HttpResponse response = HttpResponse.newBuilder()
-            .code(okResponse.code())
-            .body(okResponse.body().string())
-            .headers(headers)
-            .build();
-
-        return response;
-    }
-
-    @Override
-    @SuppressWarnings("deprecation")
-    public CompletableFuture<HttpResponse> makeRequestAsync(HttpRequest request) {
-        final CompletableFuture<HttpResponse> future = new CompletableFuture<>();
-
-        // Need to create with or without body
-
-        RequestBody okBody = request.getBody() != null ?
-            // TODO put media type on HttpRequestBody
-            RequestBody.create(MediaType.parse("application/json"), request.getBody().getContent())
-            : null;
-
-//        RequestBody okBody = request.getBody() != null ?
-//            RequestBody.create(MediaType.parse("application/json"), request.getBody())
-//            : null;
-
-
-        okhttp3.Request.Builder builder = new okhttp3.Request.Builder()
-            .url(request.getUrl())
-            .method(request.getMethod().toString(), okBody);
-        for (Map.Entry<String, String> e : request.getHeaders().entrySet()) {
-            builder.addHeader(e.getKey(), e.getValue());
-        }
-
-        // execute
-        client.newCall(builder.build()).enqueue(new Callback() {
-            @Override
-            public void onFailure(@NotNull Call call, @NotNull IOException e) {
-                future.completeExceptionally(new Auth0Exception("Failed to execute request", e));
-            }
-
-            @Override
-            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
-                try {
-                    // build Auth0 response
-                    HttpResponse aResponse = HttpResponse.newBuilder()
-                        .code(response.code())
-                        // TODO handle null string()
-                        .body(response.body().string())
-                        .build();
-                    future.complete(aResponse);
-                } catch (Auth0Exception e) {
-                    future.completeExceptionally(e);
-                }
-            }
-        });
-
-        return future;
-    }
-
-    @SuppressWarnings("deprecation")
-    private RequestBody addBody(HttpRequest request) {
-        // TODO not use null?
-        RequestBody okBody = null;
-
-        /*
-           if (request.getBody() instance of HttpMultipartRequest) {
-                MultipartBody.Builder bodyBuilder = new MultipartBody.Builder()
-                    .setType(MultipartBody.FORM);
-
-                // need FilePart type
-                bodyBuilder.addFormDataPart(request.getBody().getPartName(), request.getBody().getFile().getName(),
-                    RequestBody.create(MediaType.parse("text/json"), request.getBody().getFile());
-
-                // need Part type
-                request.getBody().get
-           }
-         */
-
-        HttpRequestBody body = request.getBody();
-        if (body != null) {
-            if (body.getMultipartRequestBody() != null) {
-                HttpMultipartRequestBody multipartRequestBody = body.getMultipartRequestBody();
-                MultipartBody.Builder bodyBuilder = new MultipartBody.Builder()
-                    .setType(MultipartBody.FORM);
-                if (multipartRequestBody.getFilePart() != null) {
-                    bodyBuilder.addFormDataPart(multipartRequestBody.getFilePart().getPartName(),
-                        multipartRequestBody.getFilePart().getFile().getName(),
-                        RequestBody.create(MediaType.parse(multipartRequestBody.getFilePart().getMediaType()), multipartRequestBody.getFilePart().getFile()));
-                }
-                multipartRequestBody.getParts().forEach(bodyBuilder::addFormDataPart);
-                okBody = bodyBuilder.build();
-            } else {
-                okBody = RequestBody.create(MediaType.parse("application/json"), request.getBody().getContent());
-            }
-        }
-
-//        if (request.getBody() != null) {
-//            // TODO put media type on requestbody!
-//            if (request.getBody().getFile() != null) {
-//                // multipart request
-//                MultipartBody.Builder bodyBuilder = new MultipartBody.Builder()
-//                    .setType(MultipartBody.FORM);
-//                // TODO need to pass name
-//                bodyBuilder.addFormDataPart("users", request.getBody().getFile().getName(),
-//                    // TODO need to pass media type
-//                    // Use deprecated method to ensure interop with okhttp 3
-//                    RequestBody.create(MediaType.parse( "text/json"), request.getBody().getFile()));
-//                request.getBody().getParams().forEach(bodyBuilder::addFormDataPart);
-//                okBody = bodyBuilder.build();
-//            } else {
-//                okBody = RequestBody.create(MediaType.parse("application/json"), request.getBody().getContent());
-//            }
-//        }
-
-//        RequestBody okBody = request.getBody() != null ?
-//            // TODO don't set content-type here? Should be on request?
-//            RequestBody.create(MediaType.parse("application/json"), request.getBody())
-//            : null;
-        return okBody;
-    }
-
     // TODO default headers?
     // TODO accept OkHttp client?
     public static class Builder {
@@ -253,10 +217,6 @@ public class DefaultHttpClient implements HttpClient {
         private LoggingOptions loggingOptions;
 
         private boolean telemetryEnabled = true;
-
-        // TODO proxy
-
-//        private final List<Interceptor> interceptors = new ArrayList<>();
 
         public Builder() {
         }
@@ -285,14 +245,6 @@ public class DefaultHttpClient implements HttpClient {
             this.telemetryEnabled = telemetryEnabled;
             return this;
         }
-
-        // TODO just take a list? Order important??
-        // TODO expose interceptors or OkHttp impl details?
-        // TODO maybe just add the logging/telemetry interceptors as part of build(), if not exposing OkHttp
-//        public Builder interceptor(Interceptor interceptor) {
-//            interceptors.add(interceptor);
-//            return this;
-//        }
 
         public DefaultHttpClient build() {
             return new DefaultHttpClient(this);
