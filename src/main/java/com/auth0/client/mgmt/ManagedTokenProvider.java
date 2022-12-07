e4cffdc09479ed79c8d5a4cc6d6a7cef171dfc41
Jim Anderson <jimranderson@gmail.com>
2022-12-07 10:49:49 -0600

    code review feedback updates

--- src/main/java/com/auth0/client/mgmt/ManagedTokenProvider.java
+++ src/main/java/com/auth0/client/mgmt/ManagedTokenProvider.java
@@ -11,7 +11,7 @@ import java.time.Instant;
 import java.util.Objects;
 
 /**
- * An implementation of {@link TokenProvider} that fetches, stores (in-memory), and renews Auth0 Management API tokens.
+ * An implementation of {@link TokenProvider} that fetches, maintains, and renews Auth0 Management API tokens.
  */
 public class ManagedTokenProvider implements TokenProvider {
 
